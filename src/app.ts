4e4ad07a35c47384da45f7a30615e94ed5ecf9e6
valedobrandi <valedobrandi@gmail.com>
2024-07-22 10:48:36 -0300

    add swagger ui

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,22 @@
 import express, { Response, Request, NextFunction } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import yaml from 'yaml';
+import path from 'path';
 import productsRouter from './routers/products.router';
 import { Error } from './types/Error';
 import usersRouter from './routers/users.route';
 import loginRouter from './routers/login.router';
 
+const filePathResolve = path.resolve(__dirname, './document/trybersmith.yaml');
+const file = fs.readFileSync(filePathResolve, 'utf8');
+const swaggerDocument = yaml.parse(file);
+
 const app = express();
 
 app.use(express.json());
 
-app.get('/', (_request, response) => {
-  response.sendStatus(200);
-});
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use('/products', productsRouter);
 app.use('/users', usersRouter);
